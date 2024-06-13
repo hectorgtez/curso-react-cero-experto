33709d38952cf8a70c4314d8ef403a950d63db73
Hector Gutierrez <hectorgtez@gmail.com>
2024-06-13 11:53:56 -0700

    [ADD] GifExpertApp - Mostrar GIFs, custom hook useFetchGif y archivo de barril

--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getGifs = async (category) => {
   const apiKey = import.meta.env.VITE_apiKey;
 
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ category }&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ category }&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
   
@@ -11,5 +11,5 @@ export const getGifs = async (category) => {
     url: img.images.downsized_medium.url
   }));
 
-  console.log(gifs);
+  return gifs;
 }
