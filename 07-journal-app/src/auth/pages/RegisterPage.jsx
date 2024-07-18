594110536f0abd78abea266ce792f9a67c15ef21
Hector Gutierrez <hectorgtez@gmail.com>
2024-07-18 11:30:43 -0700

    [ADD] JournalApp - RegisterPage, JournalPage, Navbar y Sidebar

--- 07-journal-app/src/auth/pages/RegisterPage.jsx
+++ 07-journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,85 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
+
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title='Create account'>
+      <form>
+        <Grid container>
+          {/* Full Name */}
+          <Grid
+            item
+            xs={ 12 }
+            sx={{ mt: 2 }}
+          >
+            <TextField
+              label='Full Name'
+              type='text'
+              placeholder='John Doe'
+              fullWidth
+            />
+          </Grid>
+
+          {/* Email */}
+          <Grid
+            item
+            xs={ 12 }
+            sx={{ mt: 2 }}
+          >
+            <TextField
+              label='Email'
+              type='email'
+              placeholder='example@email.com'
+              fullWidth
+            />
+          </Grid>
+
+          {/* Password */}
+          <Grid
+            item
+            xs={ 12 }
+            sx={{ mt: 2 }}
+          >
+            <TextField
+              label='Password'
+              type='password'
+              fullWidth
+            />
+          </Grid>
+
+          {/* Buttons */}
+          <Grid
+            container
+            spacing={ 2 }
+            sx={{ mb: 2, mt: 1 }}
+          >
+            <Grid
+              item
+              xs={ 12 }
+            >
+              <Button variant='contained' fullWidth>
+                Create account
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid
+            container
+            direction='row'
+            justifyContent='end'
+          >
+            <Typography sx={{ mr: 1 }}>Already have an account?</Typography>
+            <Link
+              component={ RouterLink }
+              color='inherit'
+              to='/auth/login'
+            >
+              Sign In
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
---
+++ 07-journal-app/src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,33 @@
+import { Box } from '@mui/system';
+import { Toolbar } from '@mui/material';
+import PropTypes from 'prop-types';
+
+import { Navbar, Sidebar } from '../components';
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Navbar drawerWidth={ drawerWidth }/>
+
+      <Sidebar drawerWidth={ drawerWidth }/>
+
+      <Box
+        component='main'
+        sx={{
+          flexGrow: 1,
+          p: 3
+        }}
+      >
+        <Toolbar/>
+
+        { children }
+      </Box>
+    </Box>
+  )
+}
+
+JournalLayout.propTypes = {
+  children: PropTypes.array,
+}
