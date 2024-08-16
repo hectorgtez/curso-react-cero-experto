e3855c9421410df725daf123ccbc09ce15086f0c
Hector Gutierrez <hectorgtez@gmail.com>
2024-08-16 10:36:37 -0700

    [ADD] Calendar Backend - Creacion de proyecto y endpoints de auth

---
+++ 09-calendar-backend/controllers/auth.js
@@ -0,0 +1,44 @@
+const { response } = require('express');
+
+const createUser = (req, res = response) => {
+  const { name, email, password } = req.body;
+
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: 'Name must be 5 letters long'
+    })
+  }
+
+  res.status(201).json({
+    ok: true,
+    msg: 'Register',
+    name,
+    email,
+    password,
+  });
+}
+
+const loginUser = (req, res = response) => {
+  const { email, password } = req.body;
+
+  res.json({
+    ok: true,
+    msg: 'Login',
+    email,
+    password,
+  });
+}
+
+const renewToken = (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: 'Renew'
+  });
+}
+
+module.exports = {
+  createUser,
+  loginUser,
+  renewToken,
+}
